da44f2f13b066b6620fae498ff1ca8000928df6d
amanzholtanat <amanzholtanat@gmail.com>
2022-11-08 12:45:03 +0600

    adding for loop

--- src/main/java/org/example.java
+++ src/main/java/org/example.java
@@ -2,6 +2,8 @@ package org;
 
 public class example {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 0; i<10; i++){
+            System.out.println("Hello World!");
+        }
     }
 }
